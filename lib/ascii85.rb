0bcb0d75a02b38bf6c9659806ee64c755b2b2db4
Johannes Holzfuß <Drangon@gmx.de>
2009-02-14 15:13:14 +0100

    Initial import

---
+++ lib/ascii85.rb
@@ -0,0 +1,144 @@
+
+class Ascii85
+
+  def self.encode(str, wrap_lines = 80)
+
+    return '' if str.to_s.empty?
+
+    # Compute number of \0s to pad the message with (0..3)
+    padding_length = (4 - (str.to_s.length % 4)) % 4
+
+    # Extract big-endian integers
+    tuples = (str.to_s + ("\0" * padding_length)).unpack('N*')
+
+    # Encode
+    tuples.map! do |tuple|
+      if tuple == 0
+        'z'
+      else
+        tmp = ""
+        5.times do
+          tmp += ((tuple % 85) + 33).chr
+          tuple /= 85
+        end
+        tmp.reverse
+      end
+    end
+
+    # We can't use the z-abbreviation if we're going to cut off padding
+    if (padding_length > 0) and (tuples.last == 'z')
+      tuples[-1] = '!!!!!'
+    end
+
+    # Cut off the padding
+    tuples[-1] = tuples[-1][0..(4 - padding_length)]
+
+    # Add start-marker and join into a String
+    result = '<~' + tuples.join
+
+    # If we don't need to wrap the lines to a certain length, add ~> and return
+    if (!wrap_lines)
+      return result + '~>'
+    end
+
+    # Otherwise we wrap the lines
+
+    line_length = [2, wrap_lines.to_i].max
+
+    wrapped = []
+    0.step(result.length, line_length) do |index|
+      wrapped << result.slice(index, line_length)
+    end
+
+    # Add end-marker -- on a new line if necessary
+    if (wrapped.last.length + 2) > line_length
+      wrapped << '~>'
+    else
+      wrapped[-1] += '~>'
+    end
+
+    return wrapped.join("\n")
+  end
+
+  def self.decode(str)
+
+    # Find the Ascii85 encoded data between <~ and ~>
+    input = str.to_s.match(/<~.*?~>/mn)
+
+    return '' if input.nil?
+
+    # Remove the delimiters
+    input = input.to_s[2..-3]
+
+    return '' if input.empty?
+
+    # Decode
+    result = []
+
+    count = 0
+    word = 0
+
+    input.each_byte do |c|
+
+      case c.chr
+      when /[ \t\r\n\f\0]/
+        # Ignore whitespace
+        next
+
+      when 'z'
+        if count == 0
+          # Expand z to 0-word
+          result << 0
+        else
+          raise(Ascii85::DecodingError, "found 'z' inside Ascii85 5-tuple")
+        end
+
+      when '!'..'u'
+        # Decode 5 characters into a 4-byte word
+        word += (c - 33) * 85**(4 - count)
+        count += 1
+
+        if count == 5
+
+          if word >= 2**32
+            raise(Ascii85::DecodingError,
+                  "Invalid Ascii85 5-tuple (#{word} >= 2**32)")
+          end
+
+          result << word
+          word = 0
+          count = 0
+        end
+
+      else
+        raise(Ascii85::DecodingError,
+              "Illegal character inside Ascii85: #{c.chr.dump}")
+      end
+
+    end
+
+    # Convert result into a String
+    result = result.pack('N*')
+
+    if count > 0
+      # Finish last, partially decoded 32-bit-word
+
+      if count == 1
+        raise(Ascii85::DecodingError,
+              "Last 5-tuple consists of single character")
+      end
+
+      count -= 1
+      word += 85**(4 - count)
+
+      result += ((word >> 24) & 255).chr if count >= 1
+      result += ((word >> 16) & 255).chr if count >= 2
+      result += ((word >>  8) & 255).chr if count == 3
+    end
+
+    return result
+  end
+
+end
+
+class Ascii85::DecodingError < StandardError; end
---
+++ spec/ascii85_spec.rb
@@ -0,0 +1,152 @@
+# encoding: utf-8
+
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib')
+
+require 'ascii85'
+
+describe Ascii85 do
+
+  TEST_CASES = {
+
+    ""          => "",
+    " "         => "<~+9~>",
+
+    "\0" * 1    => "<~!!~>",
+    "\0" * 2    => "<~!!!~>",
+    "\0" * 3    => "<~!!!!~>",
+    "\0" * 4    => "<~z~>",
+    "\0" * 5    => "<~z!!~>",
+    "A\0\0\0\0" => "<~5l^lb!!~>", # No z-abbreviation!
+
+    "A"         => "<~5l~>",
+    "AB"        => "<~5sb~>",
+    "ABC"       => "<~5sdp~>",
+    "ABCD"      => "<~5sdq,~>",
+    "ABCDE"     => "<~5sdq,70~>",
+    "ABCDEF"    => "<~5sdq,77I~>",
+    "ABCDEFG"   => "<~5sdq,77Kc~>",
+    "ABCDEFGH"  => "<~5sdq,77Kd<~>",
+    "ABCDEFGHI" => "<~5sdq,77Kd<8H~>",
+    "Ascii85"   => "<~6$$OMBfIs~>",
+
+    'Antidisestablishmentarianism' => '<~6#LdYA8-*rF*(i"Ch[s(D.RU,@<-\'jDJ=0/~>',
+
+    # Dōmo arigatō, Mr. Roboto (according to Wikipedia)
+    'どうもありがとうミスターロボット' =>
+        "<~j+42iJVN3:K&_E6j+<0KJW/W?W8iG`j+EuaK\"9on^Z0sZj+FJoK:LtSKB%T?~>",
+
+    [Math::PI].pack('G') => "<~5RAV2<(&;T~>",
+    [Math::E].pack('G')  => "<~5R\"n0M\\K6,~>"
+  }
+
+  it "#decode should be the inverse of #encode" do
+
+    # Generate a random string
+    test_str = ""
+    (1 + rand(255)).times do
+      test_str += rand(256).chr
+    end
+
+    encoded = Ascii85.encode(test_str)
+    decoded = Ascii85.decode(encoded)
+
+    decoded.should == test_str
+  end
+
+  describe "#encode" do
+
+    it "should encode all specified test-cases correctly" do
+      TEST_CASES.each_pair do |input, encoded|
+        Ascii85.encode(input).should == encoded
+      end
+    end
+
+    it "should produce output lines no longer than specified" do
+      test_str = '0123456789' * 30
+
+      #
+      # No wrap
+      #
+      Ascii85.encode(test_str, false).count("\n").should == 0
+
+      #
+      # x characters per line, except for the last one
+      #
+      x = 2 + rand(255) # < test_str.length
+      encoded = Ascii85.encode(test_str, x)
+
+      # Determine the length of all lines
+      count_arr = []
+      encoded.each_line do |line|
+        count_arr << line.chomp.length
+      end
+
+      # The last line is allowed to be shorter than x, so remove it
+      count_arr.pop if count_arr.last <= x
+
+      # If the end-marker is on a line of its own, the next-to-last line is
+      # allowed to be shorter than specified by exactly one character
+      count_arr.pop if (encoded[-3].chr =~ /[\r\n]/) and (count_arr.last == x-1)
+
+      # Remove all line-lengths that are of length x from count_arr
+      count_arr.delete_if { |len| len == x }
+
+      # Now count_arr should be empty
+      count_arr.should be_empty
+    end
+
+    it "should not split the delimiters to achieve correct line length" do
+      Ascii85.encode("\0" * 4, 4).should == "<~z\n~>"
+    end
+
+  end
+
+  describe "#decode" do
+
+    it "should decode all specified test-cases correctly" do
+      TEST_CASES.each_pair do |decoded, input|
+        Ascii85.decode(input).should == decoded
+      end
+    end
+
+    it "should ignore everything before/after the delimiter-pairs" do
+      Ascii85.decode("Doesn't contain delimiters").should == ''
+      Ascii85.decode("FooBar<~z~>BazQux").should == ("\0" * 4)
+    end
+
+    it "should ignore whitespace" do
+      decoded = Ascii85.decode("<~6   #LdYA\r\08\n  \n\n- *rF*(i\"Ch[s \t(D.RU,@ <-\'jDJ=0\f/~>")
+      decoded.should == 'Antidisestablishmentarianism'
+    end
+
+    describe "Error conditions" do
+
+      it "should raise DecodingError if it encounters a word >= 2**32" do
+        lambda {
+          Ascii85.decode('<~s8W-#~>')
+        }.should raise_error Ascii85::DecodingError
+      end
+
+      it "should raise DecodingError if it encounters an invalid character" do
+        lambda {
+          Ascii85.decode('<~!!y!!~>')
+        }.should raise_error Ascii85::DecodingError
+      end
+
+      it "should raise DecodingError if the last tuple consists of a single character" do
+        lambda {
+          Ascii85.decode('<~!~>')
+        }.should raise_error Ascii85::DecodingError
+      end
+
+      it "should raise DecodingError if a z is found inside a 5-tuple" do
+        lambda {
+          Ascii85.decode('<~!!z!!~>')
+        }.should raise_error Ascii85::DecodingError
+      end
+
+    end
+
+  end
+
+end
